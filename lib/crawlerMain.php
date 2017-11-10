8f8eaff4e3302f5a62157f911d6f10eee6a14a28
Zak O Leary <x15515587@student.ncirl.ie>
2017-11-10 15:37:08 +0000

    added website and Highcharts

---
+++ lib/crawlerMain.php
@@ -0,0 +1,131 @@
+<?php
+
+//Couple changes need ot be made.
+//user input for the crawler.
+//needs to stop after a certain point
+//exclude certain links
+//traverse websites method should be altered.
+//trimming titles etc.
+//Character encoding should be looked at.
+//proper export to json for database import 
+
+//url of the page being tested. 
+//this will be changed to the input url of the customers website.
+$start = "https://metal-archives.com";
+
+$already_crawled = array();
+$crawling = array();
+
+function get_details($url){
+    $options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: crawlerBot\n"));
+    $context = stream_context_create($options);
+
+    //getting the dom
+    $doc = new DOMDocument();
+    @$doc->loadHTML(@file_get_contents($url, false, $context));
+
+    $title = $doc->getElementsByTagName("title");
+    $title = $title->item(0)->nodeValue;
+
+    //echo $title."\n";
+
+    $description = "";
+    $keywords = "";
+    $metas = $doc->getElementsByTagName("meta");
+    for($i = 0; $i < $metas->length; $i++){
+        $meta = $metas->item($i);
+
+        if(strtolower($meta->getAttribute("name") == "description"))
+            $description = $meta->getAttribute("content");    
+       
+        if(strtolower($meta->getAttribute("name") == "keywords"))
+            $keywords = $meta->getAttribute("content");
+        
+    }
+    //return '{ "Title": "'.str_replace("\n", "", $title).'", "Description": "'.str_replace("\n", "", $description).'", "Keywords": "'.str_replace("\n", "", $keywords).'", "URL": "'.$url.'"},';
+    global $crawlResult;
+    $crawlResult = array(
+        'Title' => $title,
+        'Description' => $description,
+        'Keywords' => $keywords,
+        'Url' => $url);
+    
+}
+
+function follow_links($url){
+
+    global $already_crawled;
+    global $crawling;
+
+    $options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: crawlerBot\n"));
+
+    $context = stream_context_create($options);
+
+    //getting the dom
+    $doc = new DOMDocument();
+    @$doc->loadHTML(@file_get_contents($url, false, $context));
+
+    //getting all <a> tags on the page on the page 
+    $linkList = $doc->getElementsByTagName("a");
+
+    foreach($linkList as $link){
+        //getting the links attached to the a tags
+        $l = $link->getAttribute("href");
+        
+        //adapts to all sort of links by appending the website tunnel/directory/domain 
+        //problem with this regarding some strings being appended to that should not be.
+        if(substr($l, 0, 1) == "/" && substr($l, 0, 2) != "//"){
+            $l = parse_url($url)["scheme"]."://".parse_url($url)["host"].$l;
+        }
+        else if(substr($l, 0, 2) == "//"){
+            $l = parse_url($url)["scheme"].":".$l;
+        }
+        else if(substr($l, 0, 2) == "./"){
+            $l = parse_url($url)["scheme"]."://".parse_url($url)["host"].dirname(parse_url($url)["path"]).substr($l, 1);
+        }
+        else if(substr($l, 0, 1) == "#"){
+            $l = parse_url($url)["scheme"]."://".parse_url($url)["host"].parse_url($url)["path"].$l;
+        }
+        else if(substr($l, 0, 3) == "../"){
+            $l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
+        }
+        else if(substr($l, 0, 11) == "javscript:"){
+            continue;
+        }
+        else if(substr($l, 0, 5) != "https" && substr($l, 0, 4) != "http"){
+            $l = parse_url($url)["scheme"]."://".parse_url($url)["host"].dirname(parse_url($url)["path"]).$l;
+        }
+
+        if(!in_array($l, $already_crawled)){
+            $already_crawled[] = $l;
+            $crawling[] = $l;
+            echo get_details($l)."\n";
+
+            global $crawlResult;
+            $crawlExport = json_encode($crawlResult);
+            
+            //test to see if the file would write properly
+            if(file_put_contents("jacktest.json", $crawlExport, FILE_APPEND)){
+                //echo "Json file created successfully". PHP_EOL;
+                $fileCreated = true;
+            }
+            else{
+                echo "Error creating file";
+            }
+            //echo $l."\n";
+        }
+    }
+    if($fileCreated){
+        echo "Json file created successfully".PHP_EOL;
+        //the file is finished writing successfully.
+    }
+    array_shift($crawling);
+    foreach($crawling as $site){
+        follow_links($site);
+    }
+}
+
+follow_links($start);
+
+print_r($already_crawled);
+?>
---
+++ lib/scraper.php
@@ -0,0 +1,81 @@
+<?php
+// Run this file from cli every 5-10 minutes
+// doesn't matter if it takes 20-30 seconds
+$time_start = microtime(true);
+  
+
+require 'simple_html_dom.php';
+ $i = 0;
+$html_output = ""; // use this to build up html output
+  for($j=1;$j<27;$j++){
+
+
+$sites = array(
+ 
+  array('https://www.google.ie/search?q=TEST&dcr=0&ei=JQjJWf6QD-mcgAb3-qXQAw&start='.$j.'0&sa=N&biw=1920&bih=901', 'cite'),
+    /* more sites go here, like this */
+    // array('URL', 'KEY')
+);
+
+  
+  
+
+// loop over each site
+foreach ($sites as $site){
+  
+   $url = $site[0];
+   $key = $site[1];
+   // fetch site
+   $syds = file_get_html($url);
+   $test = array();
+   // loop over each link
+  
+   foreach($syds->find($key) as $element) {
+     // add link to $html_output
+    
+     $html_output .= $element->innertext . '<br>'.$i++;
+
+   }
+}
+  }
+  
+
+// save $html_output to a local file
+if(file_exists("links.php")){
+  file_put_contents("links.php", $html_output, FILE_APPEND);
+
+
+}else {
+  echo "err";
+}
+// Display Script End time
+$time_end = microtime(true);
+
+//dividing with 60 will give the execution time in minutes other wise seconds
+$execution_time = ($time_end - $time_start)/60;
+
+//execution time of the script
+echo '<b>Total Execution Time:</b> '.$execution_time.' Mins';
+
+function convert($size)
+{
+    $unit=array('b','kb','mb','gb','tb','pb');
+    return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+}
+
+echo convert(memory_get_usage(true)); // 123 kb
+
+function outputProgress($current, $total) {
+    echo "<span style='position: absolute;z-index:$current;background:#FFF;'>" . round($current / $total * 100) . "% </span>";
+    myFlush();
+    sleep(1);
+}
+
+function progressBar($done, $total) {
+    $perc = floor(($done / $total) * 100);
+    $left = 100 - $perc;
+    $write = sprintf("\033[0G\033[2K[%'={$perc}s>%-{$left}s] - $perc%% - $done/$total", "", "");
+    fwrite(STDERR, $write);
+}
+
+?>
