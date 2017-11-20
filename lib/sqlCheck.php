26740010024c66cdc79156ee18171064f69e7454
Jack <jackbourkemckenna>
2017-11-20 20:20:22 +0200

    Check webpage for the keyword sql. Need to add more keywords.

--- lib/sqlCheck.php
+++ lib/sqlCheck.php
@@ -1,46 +1,27 @@
 <?php
 $ch = curl_init();
-
 $startUrl = "http://alphaonenow.org/info.php?id=13";
-
 //add in a ' to the quotations in the append url bit. 
 $appendUrl = $startUrl."'";
-
 echo $appendUrl.PHP_EOL;
-
 curl_setopt($ch, CURLOPT_URL, $appendUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-
-global $data;
 $data = curl_exec($ch);
-
 if($data === FALSE){
 	echo "curl error " . curl_error($ch);
 }
-
-$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-curl_close($ch);
-
-
-function checkForVulnerability($data){
-	if(stristr($data, "sql syntax") === FALSE){
-		return "nope";
-	}
-	else{
-		return "sql vulnerable";
-	}
+$text = curl_exec($ch);
+$errorCheck = strpos($text, "SQL");
+if ($errorCheck==false)
+{
+    echo "no";
 }
-
-//echo ($httpcode >= 200 && $httpcode < 300) ? $data : false;
-
-checkForVulnerability($data);
-
-/*
-if(stristr($data, "SQL syntax")){
-	echo "Found";
+else
+{
+    echo "<br>SQL ERROR FOUND :D";
 }
-*/
+#https://stackoverflow.com/questions/9576772/check-if-string-exists-in-a-web-page
+#code^^^
 ?>
