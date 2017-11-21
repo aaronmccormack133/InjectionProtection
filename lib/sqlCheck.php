6b021d181a162038018de7bf4bed53ad249bf174
aaronmccormack15533 <maron.accormack@gmail.com>
2017-11-21 22:19:42 +0000

    Added mysql set of keywords

--- lib/sqlCheck.php
+++ lib/sqlCheck.php
@@ -1,27 +1,62 @@
 <?php
 $ch = curl_init();
+
 $startUrl = "http://alphaonenow.org/info.php?id=13";
 //add in a ' to the quotations in the append url bit. 
 $appendUrl = $startUrl."'";
+
 echo $appendUrl.PHP_EOL;
+
 curl_setopt($ch, CURLOPT_URL, $appendUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+
 $data = curl_exec($ch);
+
 if($data === FALSE){
 	echo "curl error " . curl_error($ch);
 }
+
 $text = curl_exec($ch);
+/*
 $errorCheck = strpos($text, "SQL");
-if ($errorCheck==false)
-{
+
+if ($errorCheck==false){
     echo "no";
 }
-else
-{
+else{
     echo "<br>SQL ERROR FOUND :D";
 }
+ */
+global $mySqlArr;
+$mySqlArr = array("SQL syntax.*?MySQL", 
+    "Warning.*?mysql_", 
+    "MySqlException \(0x", 
+    "valid MySql result", 
+    "check the manual that corresponds to your (MySQL|MariaDB) server version", 
+    "MySqlClient", 
+    "com\.mysql\.jdbc\.exceptions"
+);
+
+function checkingPage($data, $mySqlArr){
+    if(is_array($mySqlArr)){
+        foreach($mySqlArr as $mySql){
+            if(is_array($mySql)){
+                $pos = checkingPage($data, $mySql);
+            }
+            else{
+                $pos = strpos($data, $mySql);
+            }
+        }
+    }
+    else{
+        return strpos($data, $mySqlArr);
+    }
+}
+
+echo checkingPage($data, $mySqlArr);
+
 #https://stackoverflow.com/questions/9576772/check-if-string-exists-in-a-web-page
 #code^^^
 ?>
