3ea1685493fa97315727acfad64615aac20b6f7d
Jack <jackbourkemckenna>
2017-11-28 02:13:17 +0200

    Python script finsed just need to put it on a pie and let it run for ever

--- python/login.py
+++ python/login.py
@@ -6,30 +6,41 @@ import csv
 from selenium.webdriver import ActionChains
 user = []
 pw = []
-
+xpath = []
 import csv
-with open('Logins.csv') as f:
-    reader = csv.reader(f)
-    next(f);
-    for row in reader:
-        user = (row[0])
-        pw = (row[1])
-print(pw)
+test = True 
+while(test == True) :
+    time.sleep(60)
+    with open('Logins.csv') as f:
+        reader = csv.reader(f)
+        next(f);
+        for row in reader:
+            user = (row[0])
+            pw = (row[1])
+            xpath = (row[2])
+            browser = webdriver.Chrome()
+            browser.get("https://codeanywhere.com/login") 
+            time.sleep(2)
 
 
-browser = webdriver.Chrome()
-browser.get("https://codeanywhere.com/login") 
-time.sleep(2)
+            username = browser.find_element_by_id("login-email")
+            password = browser.find_element_by_id("login-password")
+            username.send_keys(user) #need to pass user here but not sure how to go about since item is stored in a list 
+            password.send_keys(pw)#same with password
+            login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+            login_attempt.submit()
+            time.sleep(5)
+            run = browser.find_element_by_xpath(xpath).click()
+            time.sleep(2)
+            browser.quit()
+    pass
+    print("1 complete\n")
 
 
-username = browser.find_element_by_id("login-email")
-password = browser.find_element_by_id("login-password")
-username.send_keys("blank") #need to pass user here but not sure how to go about since item is stored in a list 
-password.send_keys("blank")#same with password
-login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
-login_attempt.submit()
-time.sleep(5)
-run = browser.find_element_by_xpath('//*[@id="3c8119e86747343eff4f94e9097657d5"]').click()
+
+
+
+
 
 
 
